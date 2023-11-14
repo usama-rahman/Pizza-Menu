6191571d92edc827c18fa530e000c92579de7300
Usama Rahman <rahmanusama390@gmail.com>
2023-11-14 06:28:48 +0600

    OK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,106 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "./App.css";
+// import { findRenderedComponentWithType } from "react-dom/test-utils";
+
+  const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "prosciutto.jpg",
+      soldOut: false,
+    },
+
+  
+  function App() {
+    return (
+      <div className="container">
+
+        {/* <Header /> */}
+        <Menu />
+      </div>
+
+    )
+  },
+  
+
+
+  
+  function Menu() {
+    return (
+      <menu className="menu">
+        <h2>Our Menu</h2>
+        <ul className="pizzas">
+          {pizzaData.map((item) => (
+            <Pizza pizzaObj={item} key={item.name} />
+          )  )}
+
+        </ul>
+      </menu>
+    )
+  },
+  
+
+
+
+
+function Pizza() {
+  return (
+    <div>
+
+<li>
+          <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+          <div>
+            <h3>{pizzaObj.name}</h3>
+            <p>{pizzaObj.ingredients}</p>
+            <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+          </div>
+        </li>
+    </div>
+  );
+},
+
+
+ 
+
+
+// React v18
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -194,7 +294,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// React before 18
-// ReactDOM.render(<App />, document.getElementById("root"));
 */
+
+
+
