d885c97b41fcb4b96bccea26210a6822fb9a2b75
Usama Rahman <rahmanusama390@gmail.com>
2023-11-15 14:49:03 +0600

    Working

--- src/index.js
+++ src/index.js
@@ -7,42 +7,42 @@ const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "focaccia.jpg",
+    photoName: "pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "margherita.jpg",
+    photoName: "pizzas/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "spinaci.jpg",
+    photoName: "pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "funghi.jpg",
+    photoName: "pizzas/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "salamino.jpg",
+    photoName: "pizzas/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "prosciutto.jpg",
+    photoName: "pizzas/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -50,15 +50,26 @@ const pizzaData = [
 function App() {
   return (
     <div className="container">
-      {/* <Header /> */}
       <Menu />
-      {/* <Footer /> */}
     </div>
   );
 }
 
 function Menu() {
-  <h2>Our Menu</h2>;
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+      <ul className="pizzas">
+        {pizzaData.map((item) => (
+          <Pizza pizzaObj={item} key={item.name} />
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
